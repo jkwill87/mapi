994a623335c1ae912b6ed86e5c9dcea10e0cb6fc
Jessy Williams <jessy@jessywilliams.com>
2017-09-01 15:51:17 -0400

    Fixes file reading in setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 # coding=utf-8
 
 from distutils.core import setup
-from io import open
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+with open('requirements.txt', 'r') as fp:
+    REQUIREMENTS = fp.read().splitlines()
 
-with open('readme.rst', 'r').read() as fp:
-    long_description = fp.read()
+with open('readme.rst', 'r') as fp:
+    LONG_DESCRIPTION = fp.read()
 
-about = {
+ABOUT = {
     'author': 'Jessy Williams',
     'author_email': 'jessy@jessywilliams.com',
     'description': 'An API for media database APIs which allows you to search '
         + 'for metadata using a simple, common interface',
     'license': 'MIT',
-    'long_description': long_description,
+    'long_description': LONG_DESCRIPTION,
     'name': 'mapi',
     'packages': ['mapi'],
-    'install_requires': required,
+    'install_requires': REQUIREMENTS,
     'url': 'https://github.com/jkwill87/mapi',
     'version': '1.0.3'
 }
 
-setup(**about)
+setup(**ABOUT)
