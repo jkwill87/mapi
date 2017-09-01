2ffe4c8fabe039519e532fec717b7823dffbeb5b
Jessy Williams <jessy@jessywilliams.com>
2017-09-01 14:04:55 -0400

    Sources requirements from external file

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@
 from distutils.core import setup
 from io import open
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+with open('readme.rst', 'r').read() as fp:
+    long_description = fp.read()
+
 about = {
     'author': 'Jessy Williams',
     'author_email': 'jessy@jessywilliams.com',
     'description': 'An API for media database APIs which allows you to search '
         + 'for metadata using a simple, common interface',
     'license': 'MIT',
-    'long_description': open('readme.rst', 'r').read(),
+    'long_description': long_description,
     'name': 'mapi',
     'packages': ['mapi'],
-    'install_requires': [
-        'appdirs',
-        'requests',
-        'requests_cache',
-        'mock;python_version<"3"',
-        'unittest2;python_version<"3"'
-    ],
+    'install_requires': required,
     'url': 'https://github.com/jkwill87/mapi',
     'version': '1.0.2'
 }
