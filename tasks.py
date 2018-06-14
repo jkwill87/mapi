4bbe96d375633191f52c322d7abacc0d9780eec7
Jessy Williams <jessy@jessywilliams.com>
2018-06-14 00:18:54 -0400

    Uses __version__.py instead of version.txt

--- tasks.py
+++ tasks.py
@@ -2,13 +2,20 @@
 # Jessy Williams (jessy@jessywilliams.com) 2018
 
 from __future__ import print_function
+
 from builtins import input
 from os import system as sh, getcwd, sep
 from sys import argv
 
 PROJECT = getcwd().split(sep)[-1].lower()
-with open('version.txt', 'r') as version_txt:
-    VERSION = float(version_txt.read(3))
+VERSION = 0
+VERSION_PATH = "%s/%s" % (PROJECT, '__version__.py')
+try:
+    # Load VERSION from __version__.py
+    exec(open(VERSION_PATH).read(), globals())
+except (IOError, TypeError):
+    print('Could not determine version!')
+    exit()
 
 
 def help():
@@ -26,21 +33,24 @@ def clean():
     ]
     sh('rm -rvf %s' % ' '.join(garbage))
 
+
 def _bump(increment):
-    new_version = VERSION + increment
+    new_version = "%0.1f" % (VERSION + increment)
     response = input('Bump from %s to %s? (y/n) ' % (VERSION, new_version))
     if not response.lower().strip().startswith('y'):
         print('aborting')
         return
     sh('pip install -q -r requirements-dev.txt')
-    with open('version.txt', 'w') as version_txt:
-        version_txt.write(str(new_version))
-    sh('git commit -am "Minor version bump"')
+
+    with open(VERSION_PATH, 'w') as version_txt:
+        version_txt.write('VERSION = %s\n' % new_version)
+    sh('git commit -am "Version bump"')
     sh('git tag %s' % new_version)
     sh('git push --tags')
     sh('./setup.py sdist bdist_wheel')
-    sh('python -m twine upload dis/%s-%s*.whl' % (PROJECT, new_version))
-    sh('rm -r *.egg-info')
+    sh('python -m twine upload dist/%s-%s*.whl' % (PROJECT, new_version))
+    clean()
+
 
 def bump_major():
     _bump(1.0)
@@ -57,8 +67,10 @@ def install():
 def uninstall():
     sh('sudo -H pip -q uninstall -y mapi')
 
+
 def test():
     sh('python -m unittest discover -v')
+
 
 def version():
     print('%s version %s ' % (PROJECT, VERSION))
@@ -73,3 +85,5 @@ task_name = argv[1] if len(argv) == 2 else None
 
 # Run task (defaulting to help in undefined)
 TASKS.get(task_name, help)()
+
+bump_minor()
